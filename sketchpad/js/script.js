be791bf8db5a3652266971513c62f2a32a6f9400
Alex Faria <simaofaria11@gmail.com>
2015-11-28 21:16:10 +0000

    almost finished

--- sketchpad/js/script.js
+++ sketchpad/js/script.js
@@ -1,20 +1,70 @@
 $(document).ready(function () {
-    var numRows = parseInt(prompt("Grid size: ", "10"), 10);
-    console.log(numRows);
-    for (var i = 0; i < numRows; i++){
-			$(".container").append("<div class='row'></div>");
+    var num = parseInt(prompt("Grid size: ", "10"), 10);
+
+    var option = 1;
+
+    $('#clear').on('click', function(){
+        switch (option) {
+            case 1:
+                $('.square').css({'opacity': '1', 'background': 'black'});
+                break;
+            case 2:
+                $('.square').css({'opacity': '1', 'background': 'black'});
+                break;
+            case 3:
+                $('.square').css({'opacity': '1', 'background': 'white'});
+                break;
+            default:
+                break;
+        }
+
+    })
+    $('#trailing').on('click', function(){
+        option = 1;
+        $('.square').css({'opacity': '1', 'background': 'black'});
+    })
+    $('#grey').on('click', function(){
+        option = 2;
+        $('.square').css({'opacity': '1', 'background': 'black'});
+    })
+    $('#random').on('click', function(){
+        option = 3;
+        $('.square').css({'background': 'white', 'opacity': '1'});
+    })
+
+    for (var i = 0; i < num; i++){
+			$(".table").append("<tr class='row'></div>");
 		};
-    for (var i = 0; i < numRows; i++) {
-            $('.row').append('<div class="square"></div>');
+    for (var i = 0; i < num; i++) {
+            $('.row').append('<td class="square"></div>');
         };
 
     $('.square').on('mouseenter', function(){
-        $(this).addClass('highlight');
+        switch (option){
+            case 1:
+                $(this).css('opacity', '0');
+                break;
+            case 2:
+                var currentOpacity = $(this).css('opacity');
+                if(currentOpacity > 0){
+                    $(this).css('opacity', currentOpacity - 0.2);
+                }
+                break;
+            case 3:
+                var newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+                console.log(newColor);
+                $(this).css('background', newColor);
+                break;
+            default:
+        }
     })
     $('.square').on('mouseleave', function(){
-        //$(this).removeClass('highlight');
+        switch (option) {
+            case 1:
+                $(this).fadeTo(500, 1);
+                break;
+            default:
+        }
     })
-    $('#clear').on('click', function(){
-        $('.square').removeClass('highlight');
-    })
+
 });
