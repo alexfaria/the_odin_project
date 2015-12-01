cf620dc755b25b2a3400abf0e1fcfbb26570e7c4
Alex Faria <simaofaria11@gmail.com>
2015-12-01 17:16:13 +0000

    pig latin solved

--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -1,7 +1,21 @@
 def translate(str)
-    if str[0].match(/[aeiou]/)
-        str + 'ay'
-    else
-        res = str[1..-1] + str[0] + 'ay'
+    words = str.split.map do |x|
+        # until the first letter is a vowel
+        until x[0].match(/[aeiou]/)
+
+            temp = x[0]
+            x = x + x[0]
+            x = x[1..-1]
+            # if the current letter is 'u' and the previous is 'q'
+            if x[0] == 'u' and temp == 'q'
+                x = x + x[0]
+                x = x[1..-1]
+            end
+        end
+        x = x + 'ay'
     end
+    # join the words from the array into a string
+    words.join(" ")
 end
+
+puts translate("The Quick Brown Fox")
--- learn_ruby/04_pig_latin/pig_latin_spec.rb
+++ learn_ruby/04_pig_latin/pig_latin_spec.rb
@@ -68,5 +68,9 @@ describe "#translate" do
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
+  xit "capitalized words are still capitalized" do
+      s = translate("The Quick Brown Fox")
+      s.should == "Ethay Ickquay Ownbray Oxfay"
+  end
 
 end
