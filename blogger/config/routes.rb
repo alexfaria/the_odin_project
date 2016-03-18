b9e20fefc7a012f0b20ce546a3d044d50b978eac
Alex Faria <simaofaria11@gmail.com>
2016-03-18 00:52:21 +0000

    Sorcery authentication complete

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
         resources :comments
     end
     resources :tags
+    resources :authors, only: [ :new, :create, :destroy ]
+    get 'login' => 'author_sessions#new'
+    get 'logout' => 'author_sessions#destroy'
     root to: 'articles#index'
 end
